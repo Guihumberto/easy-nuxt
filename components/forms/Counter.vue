c92c78c790a403ce2c517399fcf69571e07d7fc9
Humberto Gui <juninho.joao@hotmail.com>
2021-12-05 10:46:44 -0300

    auth e cad de usurs

---
+++ components/forms/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div >
+        <div>
+            <v-btn fab class="my-3 mr-2" x-small color="error" @click="decrementar"> <v-icon>mdi-minus</v-icon></v-btn>
+            <v-btn fab class="my-3" x-small color="success" @click="incrementar"><v-icon>mdi-plus</v-icon></v-btn>
+            <span>Qtd: {{ contador }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            contador: 0
+        }
+    },
+    methods: {
+        decrementar(){
+            this.contador --
+        },
+        incrementar(){
+            this.contador ++
+        }
+    }
+}
+</script>
